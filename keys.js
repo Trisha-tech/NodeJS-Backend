c09d32afe37fce4aca8cc83f4ae4e7cafeeb9149
Trisha Sahu <55338588+Trisha-tech@users.noreply.github.com>
2020-07-25 04:53:40 +0530

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-    MONGOURI: "mongodb+srv://trisha:o7nYFVRDPyJY8eMf@cluster0.mc8nq.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    MONGOURI: "mongodb+srv://trisha:<password>@cluster0.mc8nq.mongodb.net/<dbname>?retryWrites=true&w=majority",
     JWT_SECRET: "tsdrfeuyfjdbvsdjfios",
-    SENDGRID_API: "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",
-    EMAIL: "http://localhost:3000"
+    SENDGRID_API: "********************************************",
+    EMAIL: "http://localhost:*****"
 } 
